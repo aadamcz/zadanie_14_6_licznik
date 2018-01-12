38c67c63ae0dfe7225f29395b40c22185449e919
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-01-12 19:33:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-
 var Counter = React.createClass({
-
 	getInitialState: function() {
 		return {
 			counter: 0
+		};
+	},
+
+	getDefaultProps: function() {
+		return {
+			defaultProperty: "This is a default property"
 		};
 	},
 
@@ -19,14 +23,42 @@ var Counter = React.createClass({
 		});
 	},
 
+	componentWillMount: function() {
+		console.log(
+			"ComponentWillMount, Komponent za chwilę będzie zamontowany."
+		);
+	},
+	componentDidMount: function() {
+		console.log("componentDidMount, Komponent zamontowany.");
+	},
+	componentWillReceiveProps: function() {
+		console.log(
+			"componentWillReceiveProps, Komponent odbiera nowe i aktualizuje atrybuty i stany."
+		);
+	},
+	shouldComponentUpdate: function() {
+		console.log("Czy atrybut lub komponent wymaga aktualizacji?");
+		return true;
+	},
+	componentWillUpdate: function() {
+		console.log(
+			"Komponent będzie aktualizowany jeśli shouldComponentUpdate zwróci true. Nie można tutaj zmieniać stanów."
+		);
+	},
+	componentDidUpdate: function() {
+		console.log(
+			"Komponent jest aktualizowany. Można wprowadzać zmiany w DOM."
+		);
+	},
+	componentWillUnmount: function() {
+		console.log("Odmontowanie komponentu, usuwanie z DOM.");
+	},
+
 	render: function() {
 		return React.createElement(
-			"div",{},
-			React.createElement(
-				"div",
-				{},
-				"Licznik " + this.state.counter
-			),
+			"div",
+			{},
+			React.createElement("div", {}, "Licznik " + this.state.counter),
 			React.createElement(
 				"button",
 				{ onClick: this.increment },
@@ -41,6 +73,13 @@ var Counter = React.createClass({
 	}
 });
 
+var element = React.createElement(
+	"div",
+	{},
+	React.createElement(Counter, {}, "Counter No1"),
+	React.createElement(Counter, {}, "Counter No2"),
+	React.createElement(Counter, {}, "Counter No3"),
+	React.createElement(Counter, {}, "Counter No4")
+);
 
-var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById("app"));
